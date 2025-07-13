e69f21601eab0aa5b1a0cd91f8b194e18034ae1c
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-12 21:22:37 -0300

    version 1.5.8

--- src/modules/calleds/repositories/called.repository.ts
+++ src/modules/calleds/repositories/called.repository.ts
@@ -8,32 +8,67 @@ export class CalledRepository {
   }
 
   async create(){ 
-    return await this.prisma.called.create({
-      data: {
-        fkUserCustomer: "c3a37d4d-4dc9-4324-9747-ff0eb1175cde",
-        fkUserTechnical: "f72c548e-103e-480e-97e1-e627a3ba335c",
-        titleCalled: "Teste chamado novo",
-        description: "Descrição de um chamado novo para teste.",
+    // const create =  await this.prisma.called.create({
+    //   data: {
+    //     fkUserCustomer: "c3a37d4d-4dc9-4324-9747-ff0eb1175cde",
+    //     fkUserTechnical: "f72c548e-103e-480e-97e1-e627a3ba335c",
+    //     titleCalled: "Teste chamado novo",
+    //     description: "Descrição de um chamado novo para teste.",
+    //     services: {
+    //       create: [ // Fazer map para incluir mais de um services id.
+    //         {
+    //           services: {
+    //             connect: { id: "04e3239c-7e3a-43a5-8adb-4aba49094c64" }
+    //           },
+    //         },
+    //         {
+    //           services: {
+    //             connect: { id: "67dd6f63-c40f-40d0-ad08-dfbc66722561" }
+    //           }
+    //         }
+    //       ]
+    //     }
+    //   },
+    //   include: {
+    //     services: true
+    //   }
+    // })
+
+    const index =  await this.prisma.called.findMany({
+      select: {
+        updatedAt: true,
+        id: true,
+        titleCalled: true,
         services: {
-          create: [ // Fazer map para incluir mais de um services id.
-            {
-              services: {
-                connect: { id: "04e3239c-7e3a-43a5-8adb-4aba49094c64" }
-              },
-            },
-            {
-              services: {
-                connect: { id: "67dd6f63-c40f-40d0-ad08-dfbc66722561" }
+          select: {
+            services: {
+              select: {
+                id: true,
+                titleService: true,
+                price: true
               }
             }
-          ]
-        }
-      },
-      include: {
-        services: true
+          }
+        },
+        UserCustomer: {
+          select: {
+            id: true,
+            name: true,
+            role: true
+          }
+        },
+        UserTechnical: {
+          select: {
+            id: true,
+            name: true,
+            role: true
+          }
+        },
+        callStatus: true,
       }
     })
 
+    return index
   }  
      
 }
