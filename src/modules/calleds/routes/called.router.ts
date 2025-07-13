769f7da250f0eb6d5e9febb1cc055c149592e183
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-13 14:54:36 -0300

    version 1.5.11

--- src/modules/calleds/routes/called.router.ts
+++ src/modules/calleds/routes/called.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../../../middlewares/ensureAuthenticated";
-import { userAuthorization, validateUserId } from "../../../middlewares/userAuthorization";
+import { userAuthorization } from "../../../middlewares/userAuthorization";
 import { CalledsController } from "../controllers/called.controller"
 
 export const calledRouter = Router()
@@ -9,7 +9,7 @@ const calledsController = new CalledsController()
 calledRouter.use(ensureAuthenticated)
 calledRouter.post("/", userAuthorization(["admin", "customer"]), calledsController.create)
 calledRouter.get("/", userAuthorization(["admin"]), calledsController.indexAll)
-calledRouter.get("/user", calledsController.indexUser)
+calledRouter.get("/user", userAuthorization(["customer", "technical"]), calledsController.indexUser)
 calledRouter.patch("/services", userAuthorization(["technical"]), calledsController.addServices)
 calledRouter.patch("/:called",userAuthorization(["admin", "technical"]), calledsController.update)
-calledRouter.delete("/:called/:idServices", calledsController.removeServices)
+calledRouter.delete("/:called/:idServices", userAuthorization(["technical"]), calledsController.removeServices)
