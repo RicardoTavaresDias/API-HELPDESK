8d9ee3fc2040c116a1d657fdb4f63a3113523925
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-14 19:23:45 -0300

    version 1.5.15

--- src/modules/calleds/controllers/called.controller.ts
+++ src/modules/calleds/controllers/called.controller.ts
@@ -30,8 +30,11 @@ export class CalledsController {
 
       const data = result.data.map(called => {
         const priceTotal = called.services.reduce((acc, price) => acc + parseFloat(price.services.price.toString()), 0)
+        const services = called.services.map(service => ({ id: service.services.id, titleService: service.services.titleService, price: service.services.price }))
+
         return {
           ...called,
+          services,
           priceTotal
         }
       })
