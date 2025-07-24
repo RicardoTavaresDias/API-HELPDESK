0c742b6fb3f8433962fe0a87546f5b21d71a77dc
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-24 01:59:07 -0300

    version 1.5.23

--- src/modules/calleds/utils/refactor-object-data.ts
+++ src/modules/calleds/utils/refactor-object-data.ts
@@ -6,8 +6,6 @@ const refactorObjectData = (data: InputCalled[]) => {
     const priceTotal = called.services.reduce((acc, price) => acc + parseFloat(price.price.toString()), 0)
     const services = called.services.map(service => ({ id: service.fkServices, titleService: service.titleService, price: service.price }))
 
-   console.log("saida", services)
-
     return {
       ...called,
       services,
