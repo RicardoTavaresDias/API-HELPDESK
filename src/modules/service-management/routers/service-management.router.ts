d0c911aec9c8a3a208c6a3cbb20b0eecea8c00fd
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-28 14:54:51 -0300

    version 1.5.36

--- src/modules/service-management/routers/service-management.router.ts
+++ src/modules/service-management/routers/service-management.router.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { ServicesController } from "../controllers/service-management.controller";
 import { ensureAuthenticated } from "../../../middlewares/ensureAuthenticated";
 import { userAuthorization } from "../../../middlewares/userAuthorization";
+import { basePrice } from "@/libs/basePrice"
 
 export const servicesRouter = Router()
 const servicesController = new ServicesController()
 
 servicesRouter.use(ensureAuthenticated)
+servicesRouter.get("/base", (request: Request, response: Response) => { return response.json(basePrice) })
 servicesRouter.get("/", userAuthorization(["admin", "technical", "customer"]), servicesController.index)
 servicesRouter.post("/", userAuthorization(["admin"]), servicesController.create)
 servicesRouter.patch("/:id", userAuthorization(["admin"]), servicesController.update)
